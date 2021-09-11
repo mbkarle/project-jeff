0e165304e5693d43698c45aed9f5a8efe8707e7f
Matthew Karle <mkarle2178@gmail.com>
2021-09-10 22:13:35 -0400

    added first case study

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero, { HERO_DATA } from "components/home/hero";
+import Work, { WORK_DATA } from "components/home/work";
 
 const Home = () => {
   return (
     <>
       <Hero {...HERO_DATA} />
+      <Work {...WORK_DATA} />
     </>
   );
 };
---
+++ src/components/home/work/index.js
@@ -0,0 +1,75 @@
+import * as styles from "./work.module.scss";
+
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticImage } from "gatsby-plugin-image";
+import MD from "components/basics/md";
+
+const EXAMPLE_TYPE = PropTypes.shape({
+  tags: PropTypes.arrayOf(PropTypes.string),
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+});
+
+export const WORK_DATA = {
+  examples: [
+    {
+      tags: ["UX/UI Design", "Responsive Web"],
+      title: "Nudging students to give better feedback",
+      description: `I redesigned our feedback mechanics by providing students with better guidance and structure for how to give feedback, increasing the average length of feedback by **56%** and feedback comment submission by **88%**.`,
+      img: "../../../images/work/nudge.png",
+    },
+  ],
+};
+
+const Work = ({ examples }) => {
+  return (
+    <>
+      <Separator />
+      {examples.map(({ tags, title, description, img }, idx) => (
+        <div
+          key={`example-${title}`}
+          className={styles.exampleContainer + (idx % 2 === 1 ? " " + styles.left : "")}
+        >
+          <div className={styles.exampleContent}>
+            <div className={styles.tagsContainer}>
+              {tags.map((tag) => (
+                <Tag tag={tag} key={`tag-${tag}`} />
+              ))}
+            </div>
+            <MD className={styles.exampleTitle}>{title}</MD>
+            <MD className={styles.exampleDescription}>{description}</MD>
+          </div>
+          <div className={styles.imageContainer}>
+            <StaticImage
+              src={"../../../images/work/nudge.png"}
+              alt={`Image of ${title}`}
+              placeholder="blurred"
+              layout="constrained"
+            />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+Work.propTypes = {
+  examples: PropTypes.arrayOf(EXAMPLE_TYPE),
+};
+
+const Separator = () => (
+  <div className={styles.separatorContainer}>
+    <div>Select Work</div>
+    <div className={styles.separator} />
+  </div>
+);
+
+const Tag = ({ tag }) => <MD className={styles.workTag}>{tag}</MD>;
+
+Tag.propTypes = {
+  tag: PropTypes.string.isRequired,
+};
+
+export default Work;
